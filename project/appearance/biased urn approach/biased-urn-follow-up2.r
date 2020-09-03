2217fe76d49afd60f56708817f69231106c190c3
antonaltmannhome <anton321@hotmail.com>
2020-09-03 08:41:12 +0100

    moving files around

---
+++ project/appearance/biased urn approach/biased-urn-follow-up2.r
@@ -0,0 +1,96 @@
+### so just while we're testing ideas out, we'll see if we can do something intelligent with regards to fatigue. we've got chelsea's predictions on a game by game basis, can we
+# (1) prove that fatigue does lead to unexpected rests in a predictable way
+# (2) adjust our LikFunct so that fatigued players don't get downgraded after a rest (but possibly do in the build up to a rest)
+
+source('project/appearance/biased urn approach/biased-urn-game-by-game-set-up-data.r')
+
+# so step 1, load in chelsea's estimates for one of our files
+
+myTeam = 'chelsea'
+timeDownWeightCoef = 0.1
+priorStrength = 10
+fileIn = paste0(DATAPATH, 'active_player/biased_urn_output/playermle_', myTeam, '_timedownweight_', timeDownWeightCoef, '_priorstrength_', priorStrength, '.csv')
+myTeamEstimateDF = read_csv(fileIn, col_types = list(
+  team = col_character(),
+  alltimetgn = col_integer(),
+  mainpos2 = col_character(),
+  player = col_character(),
+  appearanceMle = col_double(),
+  appearanceOdds = col_double()
+))
+
+myTeamMainposTgn = unTeamMainposTgn %>%
+  filter(team == myTeam)
+myTeamTgn = unTeamTgn %>%
+  filter(team == myTeam)
+
+myList = list('vector', nrow(myTeamTgn))
+for (tbi in 1:nrow(myTeamTgn)) {
+  myList[[tbi]] = with(myTeamTgn[tbi,],
+                       GetFormationProbabilityByTeam(team, alltimetgn, 38, historicFormationDF))
+}
+myTeamTgnFormationDF = bind_rows(myList)
+
+myList = list('vector', nrow(myTeamTgn))
+# actually, instead of predicting just hte next game, maybe THIS should still predict the next block of games
+# to get rid of the resting issue
+for (tbi in 1:nrow(myTeamTgn)) {
+  myList[[tbi]] =  with(myTeamTgn[tbi,],
+                        GetExpectedPropGameByTeam(team, alltimetgn,
+                                                  myTeamEstimateDF, myTeamTgnFormationDF))
+}
+myTeamExpectedPropGameDF = bind_rows(myList)
+
+subGbgDF = gbgdf2 %>%
+  filter(team == myTeam) %>%
+  left_join(myTeamExpectedPropGameDF, c('player', 'team', 'mainpos2', 'alltimetgn')) %>%
+  lazy_left_join(resultDF, c('team', 'alltimetgn'), 'season')
+
+calibplot(subGbgDF$expectedPropGame, subGbgDF$minute/94)
+# looks pretty solid to me
+
+# now, what about a fatigue measure?
+
+# we've got the champs league stuff to load in too somewhere
+euroFixtureDF = read_csv(paste0(DATAPATH, 'euro-fixtures-historic.csv'), col_types = list(
+  tournament = col_character(),
+  date = col_integer(),
+  team = col_character()
+)) %>%
+  filter(date > 20160800)
+
+seasonInfoDF$ymdStart = ymd(seasonInfoDF$start)
+seasonInfoDF$ymdEnd = ymd(seasonInfoDF$end)
+euroFixtureDF$ymdDate = ymd(euroFixtureDF$date)
+
+expandedSeasonInfoDateVec = c(seasonInfoDF$ymdStart[-1], tail(seasonInfoDF$ymdEnd, 1))
+expandedSeasonInfoSeasonVec = seasonInfoDF$season[-1]
+euroFixtureDF$season = expandedSeasonInfoSeasonVec[findInterval(euroFixtureDF$ymdDate, expandedSeasonInfoDateVec)]
+
+# so let's take a regular and get his list of days available and played
+
+myPlayer = 'eden hazard'
+mySeason = 1718
+miniGbgDF = subGbgDF %>%
+  filter(season == mySeason & player == myPlayer)  %>%
+  left_join(myTeamExpectedPropGameDF, c('player', 'team', 'mainpos2', 'alltimetgn')) %>%
+  left_join(resultDF, c('team', 'alltimetgn'), 'date') %>%
+  mutate(ymdDate = ymd(as.integer(date)))
+
+# but we want to insert the euro fixtures too. assume he played in them too
+myDateVec = miniGbgDF %>%
+  select(ymdDate, minute) %>%
+  bind_rows(euroFixtureDF %>%
+              filter(team == myTeam & season == mySeason) %>%
+              select(ymdDate) %>%
+              mutate(minute = 94)) %>%
+  arrange(ymdDate)
+
+# so for each game we want to know its intensity, number of games both before and after within various time intervals
+# don't know if we want that matrix actually. think you handle fatigue and intensity more separately than that
+# nah, too complicated that. let's do the matrix thing, it's good enough for what we want to do i think
+
+dayDiffMat = abs(matrix(rep(myDateVec$ymdDate, nrow(myDateVec)), nrow = nrow(myDateVec), byrow = FALSE) -
+      matrix(rep(myDateVec$ymdDate, nrow(myDateVec)), nrow = nrow(myDateVec), byrow = TRUE))
+# so the stuff to the left is surely more important than stuff to the right. so calculate fatigue and fixture business separately and handle separately in the formula that we eventually make
+
--- project/appearance/biased urn approach/run-biased-urn-game-by-game.r
+++ project/appearance/biased urn approach/run-biased-urn-game-by-game.r
@@ -1,66 +1,7 @@
 ## this file should do everything wrt the biased urn stuff
 
 ### let's try to figure out what the most common formations are in general
-
-source('c:/git/lineup/new-model-startup.r')
-source('project/appearance/biased-urn-funct.R')
-#library(doParallel)
-#registerDoParallel()
-
-fakeMaxTime = 1000L
-
-# need to sort out seasonNumber. why do we need 1516 in seasonInfoDF?
-seasonInfoDF$seasonNumber = match(seasonInfoDF$season, with(seasonInfoDF, sort(season[havegbg])))
-resultDF = lazy_left_join(resultDF, seasonInfoDF, 'season', 'seasonNumber')
-resultDF = resultDF %>%
-  mutate(alltimetgn = (seasonNumber - 1) * 38 + teamgamenumber)
-
-gbgdf = lazy_left_join(gbgdf,
-                       resultDF,
-                       c('season', 'team', 'teamgamenumber'),
-                       c('alltimetgn', 'inBlock'))
-
-gbgdf2 = gbgdf %>%
-  filter(!grepl('(injury|suspension)', startTime)) %>%
-  mutate(startTime2 = ifelse(!startTime %in% c('U', 'UU'), startTime, as.character(fakeMaxTime)),
-         endTime2 = ifelse(!startTime %in% c('U', 'UU'), endTime, as.character(fakeMaxTime))) %>%
-  mutate(startTime2 = as.integer(startTime2),
-         endTime2 = as.integer(endTime2))
-# except we've got the problem that players sometimes officially start the match at eg 97 minutes.
-# let's ceiling the endTime to be the greater of 94 and the biggest startTime of the match
-
-gbgdf2$mainpos2 = with(gbgdf2, case_when(
-  mainpos %in% c('D', 'DMC') ~ 'def',
-  mainpos == 'M' ~ 'mid',
-  mainpos %in% c('AM', 'FW') ~ 'att',
-  TRUE ~ 'other'
-))
-
-#gbgdf2 = gbgdf2 %>%
-#  select(season, seasonNumber, teamgamenumber, alltimetgn, team, player, startTime2, endTime2, minute, played, available, mainpos2, #inBlock)
-gbgdf2 = gbgdf2 %>%
-  select(alltimetgn, team, player, startTime2, endTime2, minute, played, available, mainpos2, inBlock)
-  
-
-historicFormationDF = MakeHistoricFormationDF(gbgdf2)
-
-unTeamMainposTgn = gbgdf2 %>%
-  distinct(team, mainpos2, alltimetgn) %>%
-  group_by(team, mainpos2) %>%
-  arrange(alltimetgn) %>%
-  mutate(alltimetgnDelta = alltimetgn - lag(alltimetgn, 1),
-         isValid = !is.na(alltimetgnDelta) & alltimetgnDelta == 1) %>%
-  filter(mainpos2 %in% c('def', 'mid', 'att') & isValid) %>%
-  select(team, mainpos2, alltimetgn) %>%
-  ungroup()
-unTeamTgn = unTeamMainposTgn %>%
-  distinct(team, alltimetgn)
-
-allTeamTgnObservedNumGame = gbgdf2 %>%
-  lazy_left_join(resultDF, c('alltimetgn', 'team'), 'maxEndTime') %>%
-  group_by(team, alltimetgn, player) %>%
-  summarise(observedNumGame = sum( (endTime2 - startTime2) / maxEndTime))
-
+source('project/appearance/biased-urn-game-by-game-set-up-data.r')
 # ok it will take 45 hours to run everybody ever
 # so i say we run 1 team at a time and build up the picture gradually
 GetAllPastMle = function(myTeam, timeDownWeightCoef, priorStrength) {
