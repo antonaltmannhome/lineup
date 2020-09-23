c8a4316c80a5bdc5eaa5c9689d85bbad9907e2e2
antonaltmannhome <58005258+antonaltmannhome@users.noreply.github.com>
2020-09-23 15:46:27 +0100

    Create data-fetch-lite.r

---
+++ data fetching/data-fetch-lite.r
@@ -0,0 +1,24 @@
+### let's have a separate file to do the data update
+source('c:/git/lineup/ff_startup.r')
+
+seasonInfoDF = read.csv(paste(DATAPATH,'seasoninfo.csv',sep=''))
+currentseason = 2021
+haveJustFinishedSeason = FALSE
+aboutToStartSeason = FALSE
+
+### then get hold of overall results - NB change 'haveJustFinishedSeason' from FALSE to TRUE after final day
+source(paste(USERPATH, 'data fetching/strip_fixture_result.r',sep=''))
+
+source(paste0(USERPATH, 'data fetching/strip_ffprice.r'))
+StripFFPrice()
+
+source(paste0(USERPATH, 'data fetching/strip-current-team-price.r'))
+NavigateAndSaveTransferPage()
+GetCleanCurrentTeamPriceDF()
+
+## finally fill in the odds
+source(paste0(USERPATH, 'data fetching/manual_strip_spreadex.r'))
+
+### ok, we've got all the raw data. now got to join the buggers all up
+
+## step 1, convert the cumulative whoscored data into match by match:
